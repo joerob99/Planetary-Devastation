ec6030daaa239522acd1873ccc1a6f88e7d0037d
dev-keo <keontewilsonrobinson@gmail.com>
2020-10-23 00:01:30 -0400

    Added custom game instance C++ class.

---
+++ Source/PlanetaryDevastation/PlanetaryDevastationGameInstance.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "PlanetaryDevastationGameInstance.generated.h"
+
+UCLASS()
+class PLANETARYDEVASTATION_API UPlanetaryDevastationGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};

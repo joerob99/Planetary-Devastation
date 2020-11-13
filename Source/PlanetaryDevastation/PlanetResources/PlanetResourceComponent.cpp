83ce5a558057972e2fb27611c1552fd6fde11445
joerob99 <josephnrobinson1999@gmail.com>
2020-11-13 16:18:40 -0500

    Added more functions and overall functionality to resource system

--- Source/PlanetaryDevastation/PlanetResources/PlanetResourceComponent.cpp
+++ Source/PlanetaryDevastation/PlanetResources/PlanetResourceComponent.cpp
@@ -18,45 +18,86 @@ void UPlanetResourceComponent::InitializeResourceList(TArray<AActor*> GivenResou
 		Resources.Add(Resource);
 	}
 
-	// Tracks the ratio of resources to health such that
+	// Tracks the ratio of resources to health
 	PlanetResourceRatio = Resources.Num() / StartPlanetHealth;
+
+	// Default values for debugging
+	if (PlanetResourceRatio <= 0.0) { PlanetResourceRatio = 0.5; }
+}
+
+void UPlanetResourceComponent::InitializeAlienList(TArray<AActor*> GivenAliens)
+{
+	for (auto& Alien : GivenAliens) {
+		Aliens.Add(Alien);
+	}
+
+	// Tracks the ratio of resources to health
+	//PlanetResourceRatio = Resources.Num() / StartPlanetHealth;
 }
 
 void UPlanetResourceComponent::AddResource(AActor* GivenResource)
 {
 	Resources.Add(GivenResource);
-
-	GrowPlanet();
 }
 
 void UPlanetResourceComponent::RemoveResource(AActor* GivenResource)
 {
 	Resources.Remove(GivenResource);
-
-	// Slowly kill the planet if there are no more resources available to it
-	if (Resources.Num() <= 0) { KillPlanet(); }
-	else { HurtPlanet(); }
+	GivenResource->Destroy();
 }
+
+void UPlanetResourceComponent::AddAlien(AActor* GivenAlien)
+{
+	Aliens.Add(GivenAlien);
+}
+
+void UPlanetResourceComponent::RemoveAlien(AActor* GivenAlien)
+{
+	Aliens.Remove(GivenAlien);
+	//GivenAlien->Destroy();
+}
+
+int32 UPlanetResourceComponent::GetPlanetHealth() {return CurrentPlanetHealth;}
+
+bool UPlanetResourceComponent::GetIsHomePlanet() {return IsHomePlanet;}
+
+int32 UPlanetResourceComponent::GetAlienNum() { return Aliens.Num(); }
+
+int32 UPlanetResourceComponent::GetResourceNum() { return Resources.Num(); }
 
 // Starts a growth/hurt loop that affects planet vegetation based on various factors
 void UPlanetResourceComponent::IntervalGrowthLoop()
 {
-	
+	GetWorld()->GetTimerManager().SetTimer(PlanetGrowTimerHandle, this, &UPlanetResourceComponent::GrowPlanet, PlanetHurtTimer / 2.0, false);
+	GetWorld()->GetTimerManager().SetTimer(PlanetHurtTimerHandle, this, &UPlanetResourceComponent::HurtPlanet, PlanetHurtTimer, false);
 }
 
 void UPlanetResourceComponent::HurtPlanet()
 {
+	CurrentPlanetHealth -= Aliens.Num();
 
+	// Clear all timers so that they can be used again
+	GetWorld()->GetTimerManager().ClearAllTimersForObject(this);
+
+	if (CurrentPlanetHealth <= MinPlanetHealth) { KillPlanet(); }
+
+	// Re-loop within the IntervalGrowthLoop();
+	IntervalGrowthLoop();
 }
 
 void UPlanetResourceComponent::GrowPlanet()
 {
+	CurrentPlanetHealth += Resources.Num();
 
+	if (CurrentPlanetHealth > MaxPlanetHealth && CurrentPlanetHealth < MaxPlanetHealth * 2.0) {
+		// Possibly do something in the future to increase or inhibit growth based on how much it goes over the planet's
+		// MaxPlanetHealth for each growth cycle
+	}
 }
 
 void UPlanetResourceComponent::KillPlanet()
 {
-
+	// Destroy planet's physical form and explode
 }
 
 
--- Source/PlanetaryDevastation/PlanetResources/PlanetResourceComponent.h
+++ Source/PlanetaryDevastation/PlanetResources/PlanetResourceComponent.h
@@ -21,42 +21,79 @@ protected:
 	UFUNCTION(BlueprintCallable, Category="Planet")
 	virtual void InitializeResourceList(TArray<AActor*> GivenResources);
 
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	virtual void InitializeAlienList(TArray<AActor*> GivenAliens);
+
 	UFUNCTION(BlueprintCallable, Category="Planet")
 	virtual void AddResource(AActor* GivenResource);
 
 	UFUNCTION(BlueprintCallable, Category="Planet")
 	virtual void RemoveResource(AActor* GivenResource);
 
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	virtual void AddAlien(AActor* GivenAlien);
+
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	virtual void RemoveAlien(AActor* GivenAlien);
+
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	virtual int32 GetPlanetHealth();
+
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	virtual bool GetIsHomePlanet();
+
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	virtual int32 GetAlienNum();
+
+	UFUNCTION(BlueprintCallable, Category = "Planet")
+	virtual int32 GetResourceNum();
+
 	// Starts a growth/hurt loop that affects planet vegetation based on various factors
+	UFUNCTION(BlueprintCallable, Category = "Planet")
 	virtual void IntervalGrowthLoop();
 
-	UFUNCTION(BlueprintCallable, Category = "Planet")
+	UFUNCTION()
 	virtual void HurtPlanet();
 
-	UFUNCTION(BlueprintCallable, Category = "Planet")
+	UFUNCTION()
 	virtual void GrowPlanet();
 
-	UFUNCTION(BlueprintCallable, Category = "Planet")
+	UFUNCTION()
 	virtual void KillPlanet();
 
 private:
-	// All of the resources confined within the planet
+	// All of the resources confined on the planet
 	TArray<AActor*> Resources;
+
+	// All of the aliens that reside on the planet
+	TArray<AActor*> Aliens;
+
+	// Handles to manage the timers
+	FTimerHandle PlanetHurtTimerHandle;
+	FTimerHandle PlanetGrowTimerHandle;
 
 	UPROPERTY(EditAnywhere, Category="Planet")
 	FString PlanetName;
 
-	UPROPERTY(EditAnywhere, Category="Planet")
+	UPROPERTY(EditAnywhere, Category="Planet", meta = (ClampMin = "50", ClampMax = "300"))
 	int32 MaxPlanetHealth = 100;
 
-	UPROPERTY(EditAnywhere, Category="Planet")
 	int32 MinPlanetHealth = 0;
 
-	UPROPERTY(EditAnywhere, Category = "Planet")
+	UPROPERTY(EditAnywhere, Category = "Planet", meta = (ClampMin = "1", ClampMax = "200"))
 	int32 StartPlanetHealth = 50;
+
+	UPROPERTY(EditAnywhere, Category = "Planet")
+	float PlanetHurtTimer = 5.0;
+
+	UPROPERTY(EditAnywhere, Category = "Planet")
+	bool IsHomePlanet = false;
 
 	// Planet resource to health ratio determined by initial resources
 	int32 PlanetResourceRatio;
+
+	// Track current health to determine whether planet goes extinct or not
+	int32 CurrentPlanetHealth = StartPlanetHealth;
 
 public:	
 	// Called every frame
